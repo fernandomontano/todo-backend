248142d775309053129bef0c4bab5862f1a6222f
Fernando Montano <fernando.montano41@gmail.com>
2022-12-01 15:03:04 -0600

    Added Functionality for User Table.

--- src/user/user.router.ts
+++ src/user/user.router.ts
@@ -14,3 +14,16 @@ userRouter.get("/", async (request: Request, response: Response) => {
     return response.status(500).json(error.message);
   }
 });
+
+// GET: A single User by Id
+userRouter.get("/:id", async (request: Request, response: Response) => {
+  const id: number = parseInt(request.params.id, 10);
+  try {
+    const user = await UserService.getUser(id);
+    return user
+      ? response.status(200).json(user)
+      : response.status(404).json("Not found");
+  } catch (error: any) {
+    return;
+  }
+});
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -5,7 +5,6 @@ type User = {
   name: string;
   email: string;
   token: string;
-  createdAt: Date;
 };
 
 export const listUsers = async (): Promise<User[]> => {
@@ -15,7 +14,62 @@ export const listUsers = async (): Promise<User[]> => {
       name: true,
       email: true,
       token: true,
-      createdAt: true,
+    },
+  });
+};
+
+export const getUser = async (id: number): Promise<User | null> => {
+  return db.user.findUnique({
+    where: {
+      id,
+    },
+  });
+};
+
+export const createUser = async (user: Omit<User, "id">): Promise<User> => {
+  const { name, email, token } = user;
+  return db.user.create({
+    data: {
+      name,
+      email,
+      token,
+    },
+    select: {
+      id: true,
+      name: true,
+      email: true,
+      token: true,
+    },
+  });
+};
+
+export const updateUser = async (
+  user: Omit<User, "id">,
+  id: number
+): Promise<User> => {
+  const { name, email, token } = user;
+  return db.user.update({
+    where: {
+      id,
+    },
+    data: {
+      name,
+      email,
+      token,
+    },
+    select: {
+      id: true,
+      name: true,
+      email: true,
+      token: true,
+    },
+  });
+};
+
+export const deleteUser = async (id: number): Promise<void> => {
+  await db.user.delete({
+    where: {
+      id,
     },
   });
 };
